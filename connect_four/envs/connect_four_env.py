abf57d85df20a1749c1d7cb9c0ff931bd4bfefeb
pachauri ryan <pachauri@cs.washington.edu>
2020-10-10 15:38:08 -0700

    implement ConnectFourEnv._find_highest_token()

--- connect_four/envs/connect_four_env.py
+++ connect_four/envs/connect_four_env.py
@@ -10,11 +10,38 @@ class ConnectFourEnv(gym.Env):
   M = 6
   N = 7
 
+  INVALID_MOVE = -1
+
   def __init__(self):
+    self.reset()
     pass
 
   def step(self, action):
+    # 
+    highest_token = self._find_highest_token(action)
+    # the location of the new token will be one above the highest token.
+    new_token_row = highest_token - 1
+
+    # Placing a token in a full column is an invalid move.
+    if new_token_row == -1:
+      self.done = True
+      return self.state, ConnectFourEnv.INVALID_MOVE, self.done, None
+
     pass
+
+  def _find_highest_token(self, column):
+    """ Finds the highest token belonging to either player in the selected column.
+
+      Args:
+        column (int): 0 ≤ column < ConnectFourEnv.N
+      Returns:
+        row (int): 0 ≤ row < ConnectFourEnv.M if there exist at least 1 token belonging to either player.
+                   -1 if there are no tokens in the column
+    """
+    # mask is a boolean array. It is true if there is a token in the given column and false otherwise.
+    mask = (self.state[:,:, column] != 0).any(axis=0)
+    # get the highest row in the given column belonging to either player.
+    return np.where(mask.any(axis=0), mask.argmax(axis=0), -1)
 
   def reset(self):
     """Resets the state of the environment and returns an initial observation.
@@ -23,7 +50,7 @@ class ConnectFourEnv(gym.Env):
         observation (object): the initial observation.
     """
     self.done = False
-    self.agent_turn = 0 # 0 means that it is Player 1's turn.
+    self.player_turn = 0 # 0 means that it is Player 1's turn.
 
     self.state = np.zeros(shape=(2, ConnectFourEnv.M, ConnectFourEnv.N))
 
--- connect_four/envs/connect_four_env_test.py
+++ connect_four/envs/connect_four_env_test.py
@@ -18,12 +18,44 @@ class TestConnectFourEnv(unittest.TestCase):
     # upon initialization, environment should not be done.
     self.assertFalse(self.env.done)
     # upon initialization, it should be Player 1's turn.
-    self.assertEqual(self.env.agent_turn, 0)
+    self.assertEqual(self.env.player_turn, 0)
     # upon initialization, board should be full of 0s.
     self.assertIsNone(np.testing.assert_array_equal(
       obs,
       np.zeros(shape=(2, ConnectFourEnv.M, ConnectFourEnv.N)),
     ))
+
+  def test_find_highest_token(self):
+    # place a token for Player 1 in the first column.
+    self.env.state[0, -1, 0] = 1
+    # fill the entire 2nd column with tokens belonging to Player 2.
+    self.env.state[1, :, 1] = np.ones(ConnectFourEnv.M)
+
+    # when there is only 1 token in the column,
+    # the highest token should be at the lowest row.
+    self.assertEqual(self.env._find_highest_token(0), ConnectFourEnv.M - 1)
+    # when the column is full,
+    # the highest token should be at the highest row.
+    self.assertEqual(self.env._find_highest_token(1), 0)
+    # when the column is empty,
+    # the highest token should be -1.
+    self.assertEqual(self.env._find_highest_token(2), -1)
+
+  def test_place_token_in_full_column(self):
+    # fill the entire 2nd column with tokens belonging to Player 2.
+    self.env.state[1, :, 1] = np.ones(ConnectFourEnv.M)
+    state_copy = self.env.state.copy()
+
+    obs, reward, done, _ = self.env.step(1)
+    # verify that the state has not changed.
+    self.assertIsNone(np.testing.assert_array_equal(
+      obs,
+      state_copy,
+    ))
+    # verify the expected reward.
+    self.assertEqual(reward, ConnectFourEnv.INVALID_MOVE)
+    # verify the environment is done.
+    self.assertTrue(done)
 
 
 if __name__ == '__main__':
