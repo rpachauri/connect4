526c32bba6daf81acfb7f04bd7857416b04eef17
pachauri ryan <pachauri@cs.washington.edu>
2020-10-12 21:09:27 -0700

    stub out Minimax agent

---
+++ connect_four/agents/minimax_agent.py
@@ -0,0 +1,18 @@
+import gym
+import connect4
+
+class Minimax():
+  """ A Minimax agent applies the Minimax algorithm up to some depth before
+  estimating the value of a state.
+  """
+  def __init__():
+    pass
+
+  def action():
+    pass
+
+  def _minimax():
+    pass
+
+  def _estimate():
+    pass
