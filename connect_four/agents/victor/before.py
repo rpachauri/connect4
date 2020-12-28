78eb76995d0dfb0b5cfc4ac9137c0830c1776d76
Ryan Pachauri <rpachauri@gmail.com>
2020-12-27 18:02:32 -0800

    Implement Before.

--- connect_four/agents/victor/before.py
+++ connect_four/agents/victor/before.py
@@ -1,15 +1,32 @@
 from connect_four.agents.victor import Board
+from connect_four.agents.victor import Claimeven
+from connect_four.agents.victor import Square
 from connect_four.agents.victor import Threat
+from connect_four.agents.victor import ThreatDirection
+from connect_four.agents.victor import Vertical
 
 
 class Before:
-    def __init__(self, threat: Threat):
-        """
+    def __init__(self, threat: Threat, verticals, claimevens):
+        """Initializes a Before instance.
 
         Args:
-            threat (Threat): a Threat.
+            threat (Threat): a Threat representing the Before group.
+            verticals (iterable<Vertical>): an iterable of Verticals which are part of the Before.
+            claimevens (iterable<Vertical>): an iterable of Claimevens which are part of the Before.
         """
-        self.threat = Threat
+        self.threat = threat
+        self.verticals = frozenset(verticals)
+        self.claimevens = frozenset(claimevens)
+
+    def __eq__(self, other):
+        if isinstance(other, Before):
+            return (self.threat == other.threat and
+                    self.verticals == other.verticals and
+                    self.claimevens == other.claimevens)
+
+    def __hash__(self):
+        return self.threat.__hash__() * 41 + self.verticals.__hash__() * 31 + self.claimevens.__hash__()
 
 
 def before(board: Board, threats):
@@ -22,4 +39,106 @@ def before(board: Board, threats):
     Returns:
         befores (iterable<Before>): an iterable of Befores for board.
     """
-    pass
+    befores = set()
+
+    for threat in threats:
+        # Skip all Vertical threats.
+        if threat.direction == ThreatDirection.vertical:
+            continue
+
+        # empty_squares is the set of all squares that belong to threat and are empty.
+        # If there is a single square that belongs in the uppermost row of board, then len(empty_squares) == 0.
+        empty_squares = empty_squares_of_before_group(board, threat)
+
+        if empty_squares:  # Only create variations if empty_squares has at least one square.
+            # Add all before variations with threat as the Before group to befores.
+            add_before_variations(board, befores, threat, empty_squares, [], [])
+
+    return befores
+
+
+def empty_squares_of_before_group(board: Board, threat: Threat):
+    """Retrieves the empty squares of a Before group if the given threat meets the conditions of a Before group.
+    Returns an empty list if the threat does not meet the conditions.
+
+    Args:
+        board (Board): a Board instance.
+        threat (Threat): a possible Before group.
+
+    Returns:
+        empty_squares (list<Square>):
+            If there exists a square with square.row == 0 in threat:
+                returns []
+            Otherwise:
+                returns a list of empty Squares in threat.
+    """
+    empty_squares = []
+    for square in threat.squares:
+        if square.row == 0:
+            return []
+        if board.is_empty(square):
+            empty_squares.append(square)
+    return empty_squares
+
+
+def add_before_variations(board: Board, befores, threat: Threat, empty_squares, verticals, claimevens):
+    """Adds all before variations with threat as the Before group to befores.
+
+    Args:
+        board (Board): a Board instance.
+        befores (set<Before>): a set of Befores we have accumulated so far for board.
+            Any new Befores we find with threat as the Before group will be added to befores.
+        threat (Threat): the Before group. Any new Befores we add to befores must use threat as their Before group.
+        empty_squares (list<Square>): a list of empty Squares in threat.
+        verticals (set<Vertical>): a set of Verticals which are part of the Before we are building.
+        claimevens (set<Claimeven>): a set of Claimevens which are part of the Before we are building.
+
+    Returns:
+
+    """
+    # This is a Recursive Backtracking algorithm.
+    # Base Case.
+    if not empty_squares:
+        if len(verticals) > 0:
+            # Only add the Before if there is at least one Vertical; otherwise, an Aftereven is better.
+            befores.add(Before(threat=threat, verticals=verticals, claimevens=claimevens))
+        return
+
+    # Recursive Case.
+    square = empty_squares.pop()  # Remove a square from empty_squares.
+    square_below = Square(row=square.row + 1, col=square.col)
+    square_above = Square(row=square.row - 1, col=square.col)
+
+    # Whether square is odd or even, we can always try to create a Vertical with square as the lower square.
+    if board.is_valid(square_above):
+        vertical = Vertical(upper=square_above, lower=square)
+        # Choose.
+        verticals.append(vertical)
+        # Recurse.
+        add_before_variations(board, befores, threat, empty_squares, verticals, claimevens)
+        # Unchoose.
+        verticals.remove(vertical)
+
+    # Depending on whether square is odd or even, we try to create a Vertical or Claimeven respectively.
+    if square.row % 2 == 1:  # square is odd.
+        if board.is_valid(square_below) and board.is_empty(square_below):
+            # Create a Vertical with square as the upper square.
+            vertical = Vertical(upper=square, lower=square_below)
+            # Choose.
+            verticals.append(vertical)
+            # Recurse.
+            add_before_variations(board, befores, threat, empty_squares, verticals, claimevens)
+            # Unchoose.
+            verticals.remove(vertical)
+    else:  # square is even.
+        # Since square is even, we are guaranteed that square_below is valid
+        # because board must have an even number of rows.
+        if board.is_empty(square_below):
+            # Create a Claimeven with square as the upper square.
+            claimeven = Claimeven(upper=square, lower=square_below)
+            # Choose.
+            claimevens.append(claimeven)
+            # Recurse.
+            add_before_variations(board, befores, threat, empty_squares, verticals, claimevens)
+            # Unchoose.
+            claimevens.remove(claimeven)
---
+++ connect_four/agents/victor/before_test.py
@@ -0,0 +1,154 @@
+import gym
+import unittest
+
+import numpy as np
+
+from connect_four.agents.victor import Before
+from connect_four.agents.victor import before
+from connect_four.agents.victor import Board
+from connect_four.agents.victor import Claimeven
+from connect_four.agents.victor import Square
+from connect_four.agents.victor import Threat
+from connect_four.agents.victor import Vertical
+from connect_four.agents.victor.before import add_before_variations
+from connect_four.envs.connect_four_env import ConnectFourEnv
+
+
+class TestBefore(unittest.TestCase):
+    def setUp(self) -> None:
+        self.env = gym.make('connect_four-v0')
+        ConnectFourEnv.M = 6
+        ConnectFourEnv.N = 7
+        self.env.reset()
+
+    def test_before(self):
+        self.env.state = np.array([
+            [
+                [0, 0, 1, 0, 0, 0, 1, ],
+                [1, 1, 0, 0, 1, 0, 1, ],
+                [0, 0, 1, 0, 0, 0, 0, ],
+                [1, 1, 0, 1, 1, 0, 1, ],
+                [0, 0, 1, 0, 0, 0, 0, ],
+                [1, 1, 0, 1, 1, 0, 1, ],
+            ],
+            [
+                [1, 1, 0, 0, 1, 0, 0, ],
+                [0, 0, 1, 1, 0, 0, 0, ],
+                [1, 1, 0, 1, 1, 0, 1, ],
+                [0, 0, 1, 0, 0, 0, 0, ],
+                [1, 1, 0, 1, 1, 0, 1, ],
+                [0, 0, 1, 0, 0, 1, 0, ],
+            ],
+        ])
+        board = Board(self.env.env_variables)
+        # With White to move, every Before group must belong to Black.
+        black_threats = board.potential_threats(1)
+
+        threat_4_3_to_4_6 = Threat(player=1, start=Square(row=4, col=3), end=Square(row=4, col=6))
+        threat_2_3_to_2_6 = Threat(player=1, start=Square(row=2, col=3), end=Square(row=2, col=6))
+        threat_1_3_to_4_6 = Threat(player=1, start=Square(row=1, col=3), end=Square(row=4, col=6))
+        threat_0_5_to_3_5 = Threat(player=1, start=Square(row=0, col=5), end=Square(row=3, col=5))
+        threat_1_5_to_4_5 = Threat(player=1, start=Square(row=1, col=5), end=Square(row=4, col=5))
+        threat_2_5_to_5_5 = Threat(player=1, start=Square(row=2, col=5), end=Square(row=5, col=5))
+        want_threats = {
+            threat_4_3_to_4_6,
+            threat_2_3_to_2_6,
+            threat_1_3_to_4_6,
+            threat_0_5_to_3_5,
+            threat_1_5_to_4_5,
+            threat_2_5_to_5_5,
+        }
+        self.assertEqual(want_threats, black_threats)
+
+        got_befores = before(board, black_threats)
+        vertical_3_5 = Vertical(upper=Square(row=3, col=5), lower=Square(row=4, col=5))
+        vertical_1_5 = Vertical(upper=Square(row=1, col=5), lower=Square(row=2, col=5))
+        vertical_2_5 = Vertical(upper=Square(row=2, col=5), lower=Square(row=3, col=5))
+
+        want_befores = {
+            Before(threat=threat_4_3_to_4_6, verticals=[vertical_3_5], claimevens=[]),
+            Before(threat=threat_2_3_to_2_6, verticals=[vertical_1_5], claimevens=[]),
+            # The Before below is excluded because it doesn't have any verticals. This makes it an Aftereven.
+            # claimeven = Claimeven(upper=Square(row=2, col=5), lower=Square(row=3, col=5))
+            # Before(threat=threat_2_3_to_2_6, verticals=[], claimevens=[claimeven]),
+            Before(threat=threat_1_3_to_4_6, verticals=[vertical_3_5], claimevens=[]),
+            Before(threat=threat_1_3_to_4_6, verticals=[vertical_2_5], claimevens=[]),
+        }
+        self.assertEqual(want_befores, got_befores)
+
+    def test_add_before_variations_diagram_6_8(self):
+        self.env.state = np.array([
+            [
+                [0, 0, 0, 0, 0, 0, 0, ],
+                [0, 0, 0, 0, 0, 0, 1, ],
+                [0, 0, 1, 0, 0, 0, 0, ],
+                [0, 0, 0, 1, 1, 0, 1, ],
+                [0, 0, 1, 0, 0, 0, 0, ],
+                [0, 0, 0, 1, 1, 0, 1, ],
+            ],
+            [
+                [0, 0, 0, 0, 0, 0, 0, ],
+                [0, 0, 0, 0, 0, 0, 0, ],
+                [0, 0, 0, 1, 1, 0, 1, ],
+                [0, 0, 1, 0, 0, 0, 0, ],
+                [0, 0, 0, 1, 1, 0, 1, ],
+                [0, 0, 1, 0, 0, 1, 0, ],
+            ],
+        ])
+        board = Board(self.env.env_variables)
+        threat_4_3_to_4_6 = Threat(player=1, start=Square(row=4, col=3), end=Square(row=4, col=6))
+
+        befores = set()
+        add_before_variations(board=board,
+                              befores=befores,
+                              threat=threat_4_3_to_4_6,
+                              empty_squares=[Square(row=4, col=5)],
+                              verticals=[],
+                              claimevens=[])
+        vertical_3_5 = Vertical(upper=Square(row=3, col=5), lower=Square(row=4, col=5))
+        want_befores = {
+            Before(threat=threat_4_3_to_4_6, verticals=[vertical_3_5], claimevens=[]),
+        }
+        self.assertEqual(want_befores, befores)
+
+    def test_add_before_variations_diagram_6_9(self):
+        self.env.state = np.array([
+            [
+                [0, 0, 0, 0, 0, 0, 0, ],
+                [0, 0, 0, 0, 0, 0, 0, ],
+                [0, 0, 0, 0, 0, 0, 0, ],
+                [0, 0, 0, 1, 0, 0, 0, ],
+                [0, 0, 1, 0, 0, 0, 0, ],
+                [0, 0, 0, 1, 0, 0, 0, ],
+            ],
+            [
+                [0, 0, 0, 0, 0, 0, 0, ],
+                [0, 0, 0, 0, 0, 0, 0, ],
+                [0, 0, 0, 0, 0, 0, 0, ],
+                [0, 0, 1, 0, 0, 0, 0, ],
+                [0, 0, 0, 1, 0, 0, 0, ],
+                [0, 0, 1, 0, 0, 0, 0, ],
+            ],
+        ])
+        board = Board(self.env.env_variables)
+        threat_2_1_to_5_4 = Threat(player=1, start=Square(row=2, col=1), end=Square(row=5, col=4))
+
+        befores = set()
+        add_before_variations(board=board,
+                              befores=befores,
+                              threat=threat_2_1_to_5_4,
+                              empty_squares=[Square(row=2, col=1), Square(row=5, col=4)],
+                              verticals=[],
+                              claimevens=[])
+        vertical_1_1 = Vertical(upper=Square(row=1, col=1), lower=Square(row=2, col=1))
+        claimeven_2_1 = Claimeven(upper=Square(row=2, col=1), lower=Square(row=3, col=1))
+        vertical_4_4 = Vertical(upper=Square(row=4, col=4), lower=Square(row=5, col=4))
+        want_befores = {
+            Before(threat=threat_2_1_to_5_4, verticals=[vertical_1_1, vertical_4_4], claimevens=[]),
+            Before(threat=threat_2_1_to_5_4, verticals=[vertical_4_4], claimevens=[claimeven_2_1]),
+        }
+        self.assertEqual(want_befores, befores)
+
+
+if __name__ == '__main__':
+    unittest.main()
