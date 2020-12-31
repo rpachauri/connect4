2bdb816cf8685ff3083513f1e697e385bad7dfe4
Ryan Pachauri <rpachauri@gmail.com>
2020-12-30 21:11:33 -0800

    Implement solution.from_aftereven()

--- connect_four/agents/victor/solution.py
+++ connect_four/agents/victor/solution.py
@@ -1,6 +1,7 @@
 from collections import namedtuple
 
 from connect_four.agents.victor import Rule
+from connect_four.agents.victor import Square
 
 from connect_four.agents.victor import Claimeven
 from connect_four.agents.victor import Baseinverse
@@ -60,7 +61,7 @@ def from_baseinverse(baseinverse: Baseinverse, squares_to_threats) -> Solution:
 
 def from_vertical(vertical: Vertical, squares_to_threats) -> Solution:
     """Converts a Vertical into a Solution.
-    Must solve at least one potential threat in order to be converted into a Solution
+    Must solve at least one potential threat in order to be converted into a Solution.
 
     Args:
         vertical (Vertical): a Vertical.
@@ -77,8 +78,106 @@ def from_vertical(vertical: Vertical, squares_to_threats) -> Solution:
         return Solution(rule=Rule.Vertical, squares=squares, threats=threats_intersection)
 
 
-def from_aftereven(aftereven: Aftereven) -> Solution:
-    pass
+def from_aftereven(aftereven: Aftereven, squares_to_threats) -> Solution:
+    """Converts an Aftereven into a Solution.
+    Must solve at least one *new* potential threat in order to be converted into a Solution.
+    By "new potential threat," we mean any threat that isn't already solved by one of the Claimevens
+    which are part of the Aftereven.
+
+    Args:
+        aftereven (Aftereven): an Aftereven.
+        squares_to_threats (Map<Square, Set<Threat>>): A dictionary mapping each
+            Square to all Threats that contain that Square.
+
+    Returns:
+        solution (Solution): a Solution if aftereven can be converted into one. None if it can't.
+    """
+    # columns_to_highest_rows is a dictionary mapping each column in the Aftereven group to the
+    # row with the highest empty Square in the Aftereven group.
+    columns_to_highest_rows = {}
+    for claimeven in aftereven.claimevens:
+        square = claimeven.upper
+        if square.col not in columns_to_highest_rows:
+            columns_to_highest_rows[square.col] = square.row
+        else:
+            columns_to_highest_rows[square.col] = min(columns_to_highest_rows[square], square.row)
+
+    # empty_squares_of_aftereven contains the empty Squares of the Aftereven group.
+    # If multiple Squares belonged to the same column, then only the Square in the highest row is included.
+    empty_squares_of_aftereven = []
+    for col in columns_to_highest_rows:
+        empty_squares_of_aftereven.append(Square(row=columns_to_highest_rows[col], col=col))
+
+    threats = set()
+    add_new_threats_from_aftereven(
+        threats=threats,
+        empty_squares_of_aftereven=empty_squares_of_aftereven,
+        threatening_squares=[],
+        squares_to_threats=squares_to_threats,
+    )
+
+    # Aftereven should only be converted into a Solution if it refutes new threats.
+    if threats:
+        squares_involved = list(aftereven.threat.squares)
+        for claimeven in aftereven.claimevens:
+            claimeven_solution = from_claimeven(claimeven, squares_to_threats)
+            threats.update(claimeven_solution.threats)
+            squares_involved.append(claimeven.lower)
+
+        return Solution(
+            rule=Rule.Aftereven,
+            squares=frozenset(squares_involved),
+            threats=frozenset(threats),
+        )
+
+
+def add_new_threats_from_aftereven(threats, empty_squares_of_aftereven, threatening_squares, squares_to_threats):
+    """Adds any new threats that intersect squares above empty_squares_of_aftereven to threats.
+    This is a recursive backtracking algorithm.
+    threatening_squares starts out as an empty list.
+    empty_squares_of_aftereven starts out as a list of all empty squares of the aftereven.
+
+    We select a square from empty_squares_of_aftereven and remove it from the list.
+    For every square above that square, we add it to threatening_squares, recurse, and then remove it from
+      threatening_squares.
+    The base case is when empty_squares_of_aftereven is empty. At that point, we find all threats that include
+      all squares in threatening_squares and add them to threats.
+
+    Args:
+        threats (set<Threat>): a Set of Threats this function will add to.
+        empty_squares_of_aftereven (list<Square>):
+            A subset of empty Squares that belong to an Aftereven.
+            If two Squares from the Aftereven belong in the same column,
+                the Square from the higher row should be given.
+            It is required that none of the Squares share the same column.
+        threatening_squares (list<Square>):
+            A list of Squares that could belong to a threat that the Aftereven refutes.
+            It is required that none of the Squares share the same column.
+        squares_to_threats (Map<Square, Set<Threat>>): A dictionary mapping each
+            Square to all Threats that contain that Square.
+
+    Returns:
+        None
+    """
+    # Base case.
+    if not empty_squares_of_aftereven:
+        new_threats_to_add = squares_to_threats[threatening_squares[0]]
+        for square in threatening_squares[1:]:
+            new_threats_to_add = new_threats_to_add.intersection(squares_to_threats[square])
+        threats.update(new_threats_to_add)
+        return
+
+    # Recursive Case.
+    square = empty_squares_of_aftereven.pop()
+    for row in range(square.row - 1, -1, -1):
+        square_above = Square(row=row, col=square.col)
+        # Choose.
+        threatening_squares.append(square_above)
+        # Recurse.
+        add_new_threats_from_aftereven(threats, empty_squares_of_aftereven, threatening_squares, squares_to_threats)
+        # Unchoose.
+        threatening_squares.remove(square_above)
+    empty_squares_of_aftereven.append(square)
 
 
 def from_lowinverse(lowinverse: Lowinverse) -> Solution:
--- connect_four/agents/victor/solution_test.py
+++ connect_four/agents/victor/solution_test.py
@@ -14,6 +14,7 @@ from connect_four.agents.victor import threat
 from connect_four.agents.victor import Claimeven
 from connect_four.agents.victor import Baseinverse
 from connect_four.agents.victor import Vertical
+from connect_four.agents.victor import Aftereven
 
 from connect_four.envs.connect_four_env import ConnectFourEnv
 
@@ -148,6 +149,71 @@ class TestSolution(unittest.TestCase):
                 Threat(player=0, start=Square(row=3, col=4), end=Square(row=0, col=4)),  # e3-e6
             ])
         )
+        self.assertEqual(want_solution, got_solution)
+
+    def test_aftereven(self):
+        # This board is from Diagram 6.5 of the original paper.
+        self.env.state = np.array([
+            [
+                [0, 0, 0, 0, 0, 0, 0, ],
+                [0, 0, 0, 0, 0, 0, 0, ],
+                [0, 1, 0, 0, 0, 0, 0, ],
+                [0, 0, 1, 1, 1, 0, 0, ],
+                [0, 1, 1, 0, 0, 0, 0, ],
+                [0, 1, 1, 1, 0, 0, 0, ],
+            ],
+            [
+                [0, 0, 0, 0, 0, 0, 0, ],
+                [0, 0, 0, 0, 0, 0, 0, ],
+                [0, 0, 1, 1, 1, 0, 0, ],
+                [0, 1, 0, 0, 0, 0, 0, ],
+                [1, 0, 0, 1, 1, 0, 0, ],
+                [1, 0, 0, 0, 1, 0, 0, ],
+            ],
+        ])
+        board = Board(self.env.env_variables)
+
+        white_threats = board.potential_threats(0)
+        square_to_threats = threat.square_to_threats(white_threats)
+
+        # The Aftereven d2-g2 solves all groups which need a square in both the f and g column.
+        aftereven_d2_g2 = Aftereven(
+            threat=Threat(player=1, start=Square(row=4, col=3), end=Square(row=4, col=6)),  # d2-g2
+            claimevens=[
+                Claimeven(upper=Square(row=4, col=5), lower=Square(row=5, col=5)),  # Claimeven f1-f2
+                Claimeven(upper=Square(row=4, col=6), lower=Square(row=5, col=6)),  # Claimeven g1-g2
+            ],
+        )
+
+        got_solution = solution.from_aftereven(aftereven_d2_g2, square_to_threats)
+        want_solution = Solution(
+            rule=Rule.Aftereven,
+            squares=frozenset([
+                # Squares from d2-g2.
+                Square(row=4, col=3),
+                Square(row=4, col=4),
+                Square(row=4, col=5),
+                Square(row=4, col=6),
+                # Lower squares of Claimevens.
+                Square(row=5, col=5),
+                Square(row=5, col=6),
+            ]),
+            threats=frozenset([
+                # New threats from Aftereven.
+                Threat(player=0, start=Square(row=3, col=3), end=Square(row=3, col=6)),  # d3-g3
+                Threat(player=0, start=Square(row=1, col=3), end=Square(row=1, col=6)),  # d5-g5
+                Threat(player=0, start=Square(row=0, col=3), end=Square(row=0, col=6)),  # d6-g6
+                Threat(player=0, start=Square(row=0, col=3), end=Square(row=3, col=6)),  # d0-g3
+                # Threats refuted by Claimeven f1-f2.
+                Threat(player=0, start=Square(row=2, col=5), end=Square(row=5, col=5)),  # f1-f4
+                Threat(player=0, start=Square(row=1, col=5), end=Square(row=4, col=5)),  # f2-f5
+                # Threats refuted by Claimeven g1-g2.
+                Threat(player=0, start=Square(row=2, col=6), end=Square(row=5, col=6)),  # g1-g4
+                Threat(player=0, start=Square(row=1, col=6), end=Square(row=4, col=6)),  # g2-g5
+            ]),
+        )
+        for got_threat in got_solution.threats:
+            print(got_threat.squares)
         self.assertEqual(want_solution, got_solution)
 
 
