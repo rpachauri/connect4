c79a3c0b218bd5010d76e1875c8864c4cb5aa153
Ryan Pachauri <rpachauri@gmail.com>
2021-01-01 10:13:50 -0800

    Stub out combination.allowed()

---
+++ connect_four/agents/victor/combination.py
@@ -0,0 +1,14 @@
+from connect_four.agents.victor import Solution
+
+
+def allowed(s1: Solution, s2: Solution):
+    """Returns True if the two Solutions can be combined; Otherwise, False.
+
+    Args:
+        s1 (Solution): a Solution.
+        s2 (Solution): a Solution.
+
+    Returns:
+        combination_allowed (bool): True if the two Solutions can be combined; Otherwise, False.
+    """
+    return False
