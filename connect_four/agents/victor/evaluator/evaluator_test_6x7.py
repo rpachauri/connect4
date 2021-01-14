1e614e10f3d771dfef31bc9b24849ffd51371d48
Ryan Pachauri <rpachauri@gmail.com>
2021-01-13 17:22:40 -0800

    Add tests for evaluator.evaluate() from Appendix B in the original paper.

---
+++ connect_four/agents/victor/evaluator/evaluator_test_6x7.py
@@ -0,0 +1,116 @@
+import gym
+import unittest
+
+import numpy as np
+
+from connect_four.agents.victor.game import Board
+from connect_four.agents.victor.evaluator import evaluator
+from connect_four.envs.connect_four_env import ConnectFourEnv
+
+
+class TestEvaluator6x7(unittest.TestCase):
+    def setUp(self) -> None:
+        self.env = gym.make('connect_four-v0')
+        ConnectFourEnv.M = 6
+        ConnectFourEnv.N = 7
+        self.env.reset()
+
+    def test_evaluate_6x7(self):
+        # The empty 6x7 board has no solution set for Black because White is guaranteed to win.
+        self.env.state = np.array([
+            [
+                [0, 0, 0, 0, 0, 0, 0, ],
+                [0, 0, 0, 0, 0, 0, 0, ],
+                [0, 0, 0, 0, 0, 0, 0, ],
+                [0, 0, 0, 0, 0, 0, 0, ],
+                [0, 0, 0, 0, 0, 0, 0, ],
+                [0, 0, 0, 0, 0, 0, 0, ],
+            ],
+            [
+                [0, 0, 0, 0, 0, 0, 0, ],
+                [0, 0, 0, 0, 0, 0, 0, ],
+                [0, 0, 0, 0, 0, 0, 0, ],
+                [0, 0, 0, 0, 0, 0, 0, ],
+                [0, 0, 0, 0, 0, 0, 0, ],
+                [0, 0, 0, 0, 0, 0, 0, ],
+            ],
+        ])
+        board = Board(self.env.env_variables)
+        got_solution_set = evaluator.evaluate(board=board)
+        self.assertIsNone(got_solution_set)
+
+    def test_evaluate_6x7_a1(self):
+        # This test case is based on Appendix B: Situation after 1. a1.
+        self.env.state = np.array([
+            [
+                [0, 0, 0, 0, 0, 0, 0, ],
+                [0, 0, 0, 0, 0, 0, 0, ],
+                [0, 0, 0, 0, 0, 0, 0, ],
+                [0, 0, 0, 0, 0, 0, 0, ],
+                [0, 0, 0, 0, 0, 0, 0, ],
+                [1, 0, 0, 0, 0, 0, 0, ],
+            ],
+            [
+                [0, 0, 0, 0, 0, 0, 0, ],
+                [0, 0, 0, 0, 0, 0, 0, ],
+                [0, 0, 0, 0, 0, 0, 0, ],
+                [0, 0, 0, 0, 0, 0, 0, ],
+                [0, 0, 0, 0, 0, 0, 0, ],
+                [0, 1, 0, 0, 0, 0, 0, ],
+            ],
+        ])
+        board = Board(self.env.env_variables)
+        got_solution_set = evaluator.evaluate(board=board)
+        self.assertIsNotNone(got_solution_set)
+
+    def test_evaluate_6x7_b1(self):
+        # This test case is based on Appendix B: Situation after 1. b1.
+        self.env.state = np.array([
+            [
+                [0, 0, 0, 0, 0, 0, 0, ],
+                [0, 0, 0, 0, 0, 0, 0, ],
+                [0, 0, 0, 0, 0, 0, 0, ],
+                [0, 0, 0, 0, 0, 0, 0, ],
+                [0, 0, 0, 0, 0, 0, 0, ],
+                [0, 1, 0, 0, 0, 0, 0, ],
+            ],
+            [
+                [0, 0, 0, 0, 0, 0, 0, ],
+                [0, 0, 0, 0, 0, 0, 0, ],
+                [0, 0, 0, 0, 0, 0, 0, ],
+                [0, 0, 0, 0, 0, 0, 0, ],
+                [0, 0, 0, 0, 0, 0, 0, ],
+                [0, 0, 1, 0, 0, 0, 0, ],
+            ],
+        ])
+        board = Board(self.env.env_variables)
+        got_solution_set = evaluator.evaluate(board=board)
+        self.assertIsNotNone(got_solution_set)
+
+    def test_evaluate_6x7_c1(self):
+        # This test case is based on Appendix B: Situation after 1. c1.
+        self.env.state = np.array([
+            [
+                [0, 0, 0, 0, 0, 0, 0, ],
+                [0, 0, 0, 0, 0, 0, 0, ],
+                [0, 0, 0, 0, 0, 0, 0, ],
+                [0, 0, 0, 0, 0, 0, 0, ],
+                [0, 0, 0, 0, 0, 0, 0, ],
+                [0, 0, 1, 0, 0, 0, 0, ],
+            ],
+            [
+                [0, 0, 0, 0, 0, 0, 0, ],
+                [0, 0, 0, 0, 0, 0, 0, ],
+                [0, 0, 0, 0, 0, 0, 0, ],
+                [0, 0, 0, 0, 0, 0, 0, ],
+                [0, 0, 0, 0, 0, 0, 0, ],
+                [0, 0, 0, 1, 0, 0, 0, ],
+            ],
+        ])
+        board = Board(self.env.env_variables)
+        got_solution_set = evaluator.evaluate(board=board)
+        self.assertIsNotNone(got_solution_set)
+
+
+if __name__ == '__main__':
+    unittest.main()
