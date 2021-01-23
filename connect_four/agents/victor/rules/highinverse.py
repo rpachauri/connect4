b4cb95aef5128bed2d358464fe5cf65a55d22bd1
Ryan Pachauri <rpachauri@gmail.com>
2021-01-22 19:55:10 -0800

    Add directly playable squares to Highinverse data structure.

--- connect_four/agents/victor/rules/highinverse.py
+++ connect_four/agents/victor/rules/highinverse.py
@@ -1,9 +1,15 @@
+from connect_four.agents.victor.game import Board
+
 from connect_four.agents.victor.rules import Lowinverse
 
 
 class Highinverse:
-    def __init__(self, lowinverse: Lowinverse):
+    def __init__(self, lowinverse: Lowinverse, directly_playable_squares=None):
         self.lowinverse = lowinverse
+
+        if directly_playable_squares is None:
+            directly_playable_squares = set()
+        self.directly_playable_squares = frozenset(directly_playable_squares)
 
     def __eq__(self, other):
         if isinstance(other, Highinverse):
@@ -14,16 +20,25 @@ class Highinverse:
         return self.lowinverse.__hash__()
 
 
-def find_all_highinverses(lowinverses):
+def find_all_highinverses(board: Board, lowinverses):
     """find_all_highinverses takes an iterable of Lowinverses and returns an iterable of Highinverses.
 
     Args:
+        board (Board): a Board instance.
         lowinverses (iterable<Lowinverse>): an iterable of Lowinverses.
 
     Returns:
         highinverses (iterable<Highinverse>): an iterable of Highinverses.
     """
+    directly_playable_squares = board.playable_squares()
     highinverses = set()
     for lowinverse in lowinverses:
-        highinverses.add(Highinverse(lowinverse))
+        highinverse_directly_playable_squares = set()
+        for vertical in lowinverse.verticals:
+            if vertical.lower in directly_playable_squares:
+                highinverse_directly_playable_squares.add(vertical.lower)
+        highinverses.add(Highinverse(
+            lowinverse=lowinverse,
+            directly_playable_squares=highinverse_directly_playable_squares,
+        ))
     return highinverses
