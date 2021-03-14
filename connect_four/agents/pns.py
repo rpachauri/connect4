44d9cd0cc59224023f9d6582573d188eeb4a0472
Ryan Pachauri <rpachauri@gmail.com>
2021-03-13 16:53:15 -0800

    Stub out helper functions of PNSNode.

--- connect_four/agents/pns.py
+++ connect_four/agents/pns.py
@@ -1,4 +1,10 @@
+# Required in order to type hint a method with the type of the enclosing class.
+# See https://stackoverflow.com/questions/33533148/how-do-i-type-hint-a-method-with-the-type-of-the-enclosing-class.
+from __future__ import annotations
+
 from connect_four.agents.agent import Agent
+
+from connect_four.evaluation import Evaluator
 from connect_four.evaluation.evaluator import ProofStatus
 
 
@@ -8,6 +14,25 @@ class PNSNode:
         self.proof = 0
         self.disproof = 0
         self.children = {}
+
+    def __eq__(self, other):
+        if isinstance(other, PNSNode):
+            return (self.status == other.status and
+                    self.proof == other.proof and
+                    self.disproof == other.disproof and
+                    self.children == other.children)
+
+    def update_tree(self, evaluator: Evaluator):
+        pass
+
+    def expand(self, evaluator: Evaluator):
+        pass
+
+    def set_proof_and_disproof_numbers(self):
+        pass
+
+    def select_most_proving_child(self) -> (int, PNSNode):
+        pass
 
 
 class PNS(Agent):
