f0c488c0126bd8b4598b4d07036854f80f577e1a
Ryan Pachauri <rpachauri@gmail.com>
2021-03-19 18:38:01 -0700

    Stub out a transposition hashing library for TicTacToe.

---
+++ connect_four/transposition/tic_tac_toe_hashing.py
@@ -0,0 +1,10 @@
+def hash_position(state) -> str:
+    """
+
+    Args:
+        state (np.ndarray): a state in the State space of TwoPlayerGameEnv.
+
+    Returns:
+        transposition (str): a string representing the transposition for state.
+    """
+    return ""
