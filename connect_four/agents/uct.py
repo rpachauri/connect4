fd5a4d9781ae30245aef831aa542a2865bc15787
Ryan Pachauri <rpachauri@gmail.com>
2021-03-20 18:04:43 -0700

    Fix indentation for uct.py.

--- connect_four/agents/uct.py
+++ connect_four/agents/uct.py
@@ -15,16 +15,16 @@ class UCTNode:
     def update_tree(self, env):
         """Performs UCT for this node and all children of this node.
 
-      Args:
-       - env is a "plannable" OpenAI gym environment.
-      Requires:
-       - env's state is at the current node
-      Effects:
-       - Creates a new leaf node.
-       - Updates the value estimate for each node along this path
-      Returns:
-       - the value of the new leaf node
-      """
+        Args:
+            - env is a "plannable" OpenAI gym environment.
+        Requires:
+            - env's state is at the current node
+        Effects:
+            - Creates a new leaf node.
+            - Updates the value estimate for each node along this path
+        Returns:
+            - the value of the new leaf node
+        """
         # SELECTION
         # Select an action using Upper Confidence Bounds.
         action = self.select_action_for_rollout()
@@ -64,13 +64,13 @@ class UCTNode:
     def rollout(env, done):
         """
 
-      Args:
-        env (gym.Env):
-        done (bool):
+        Args:
+            env (gym.Env):
+            done (bool):
 
-      Returns:
-        value (float): the total return after performing rollout from the state env is in
-      """
+        Returns:
+            value (float): the total return after performing rollout from the state env is in
+        """
         value = 0
         while not done:
             all_actions = np.arange(env.action_space)
@@ -87,9 +87,9 @@ class UCT(Agent):
     def __init__(self, num_rollouts):
         """
 
-    Args:
-      num_rollouts: the number of rollouts we simulate
-  """
+        Args:
+            num_rollouts: the number of rollouts we simulate
+        """
         self.root = None
         self.num_rollouts = num_rollouts
         self.root_num_visits = 1  # number of times we've visited the root node
@@ -97,22 +97,22 @@ class UCT(Agent):
     def action(self, env, last_action=None):
         """Returns an action.
 
-    Args:
-      env: a plannable gym.Env
-      last_action: the last_action we took
-    Requires:
-      - env must implement env_variables, which returns a variable that can
-        be passed to env.reset() to restore a state (this supports planning agents)
-      - env is a deterministic environment.
-      - action space of env is finite.
-      - current state of env MUST match one of self.root.children
-    Effects:
-      - performs a number of rollouts
-      - selects an action and moves this tree's root to the resulting state
-      - the original environment is restored if modified
-    Returns:
-      the best action after performing num_rollouts simulations
-  """
+        Args:
+            env: a plannable gym.Env
+            last_action: the last_action we took
+        Requires:
+            - env must implement env_variables, which returns a variable that can
+                be passed to env.reset() to restore a state (this supports planning agents)
+            - env is a deterministic environment.
+            - action space of env is finite.
+            - current state of env MUST match one of self.root.children
+        Effects:
+            - performs a number of rollouts
+            - selects an action and moves this tree's root to the resulting state
+            - the original environment is restored if modified
+        Returns:
+            the best action after performing num_rollouts simulations
+        """
         if self.root is None:
             # Initialize tree if this is the first time action() is being called.
             self.root = UCTNode(env.action_space)
