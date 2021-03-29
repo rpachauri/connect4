64398e56073a426ba85fa95e2c8149fdb333aa5a
Ryan Pachauri <rpachauri@gmail.com>
2021-03-28 18:43:29 -0700

    Implement TicTacToeHasher.move(). Enable initializing the TicTacToeHasher from an arbitrary state by passing in a TicTacToeEnv.

--- connect_four/hashing/tic_tac_toe_hasher.py
+++ connect_four/hashing/tic_tac_toe_hasher.py
@@ -1,4 +1,6 @@
 from collections import namedtuple
+
+from connect_four.envs import TicTacToeEnv
 from connect_four.hashing import Hasher
 from enum import Enum
 
@@ -34,7 +36,7 @@ SQUARE_TYPE_TO_SQUARE_CHAR = {
 
 class TicTacToeHasher(Hasher):
 
-    def __init__(self):
+    def __init__(self, env: TicTacToeEnv):
         """
         Assumptions:
             1.  The Hasher starts at the initial state of Tic-Tac-Toe,
@@ -62,7 +64,14 @@ class TicTacToeHasher(Hasher):
                 rows.append(SquareType.Empty)
             self.square_types.append(rows)
 
-        self.player = 0
+        state, self.player = env.env_variables
+
+        for row in range(3):
+            for col in range(3):
+                if state[0][row][col] == 1:
+                    self._play_square(player=0, row=row, col=col)
+                elif state[1][row][col] == 1:
+                    self._play_square(player=1, row=row, col=col)
 
     def move(self, action: int):
         """
@@ -72,7 +81,39 @@ class TicTacToeHasher(Hasher):
         Args:
             action (int): a valid action in the current state of Tic-Tac-Toe.
         """
-        pass
+        # Convert action into (row, col).
+        row, col = action // 2, action % 2
+
+        self._play_square(player=self.player, row=row, col=col)
+
+        # Switch play.
+        self.player = 1 - self.player
+
+    def _play_square(self, player: int, row: int, col: int):
+        # Find all Groups that belong to the opponent at that square
+        opponent = 1 - player
+        groups = self.groups_by_squares[opponent][row][col].copy()
+
+        # Change groups_by_squares to reflect that the opponent cannot win using any group in groups.
+        # Also, find all indifferent squares.
+        indifferent_squares = set()
+        for g in groups:
+            for s in g.squares:
+                self.groups_by_squares[opponent][s.row][s.col].discard(g)
+                if (not self.groups_by_squares[opponent][s.row][s.col]) and \
+                        (not self.groups_by_squares[self.player][s.row][s.col]):
+                    indifferent_squares.add(s)
+
+        # Change the square types of indifferent squares.
+        for s in indifferent_squares:
+            self.square_types[s.row][s.col] = SquareType.Indifferent
+
+        # If the played square does not immediately become an indifferent square, it belongs to the player.
+        if Square(row=row, col=col) not in indifferent_squares:
+            if player == 0:
+                self.square_types[row][col] = SquareType.Player1
+            else:
+                self.square_types[row][col] = SquareType.Player2
 
     def undo_move(self):
         """
--- connect_four/hashing/tic_tac_toe_hasher_test.py
+++ connect_four/hashing/tic_tac_toe_hasher_test.py
@@ -1,4 +1,7 @@
+import gym
 import unittest
+
+import numpy as np
 
 from connect_four.hashing import TicTacToeHasher
 from connect_four.hashing.tic_tac_toe_hasher import Square
@@ -17,9 +20,10 @@ class TestTicTacToeHasher(unittest.TestCase):
     GROUP_20_TO_02 = Group(squares=frozenset([Square(row=2, col=0), Square(row=1, col=1), Square(row=0, col=2)]))
 
     def setUp(self) -> None:
-        self.hasher = TicTacToeHasher()
+        self.env = gym.make('tic_tac_toe-v0')
+        self.hasher = TicTacToeHasher(env=self.env)
 
-    def test_init(self):
+    def test_init_initial_state(self):
         # Validate expected groups.
         want_player_0_groups_at_00 = {
             TestTicTacToeHasher.GROUP_00_TO_02,
@@ -43,6 +47,42 @@ class TestTicTacToeHasher(unittest.TestCase):
 
         # Validate current player.
         self.assertEqual(0, self.hasher.player)
+
+    def test_play_square(self):
+        self.hasher = TicTacToeHasher(env=self.env)
+        # Since no squares have been played, it is possible for Player 2 to win using Group 00-02.
+        self.assertIn(TestTicTacToeHasher.GROUP_00_TO_02, self.hasher.groups_by_squares[1][0][2])
+        self.hasher._play_square(player=0, row=0, col=0)
+        # Since Player 1 has played at 00, it is not possible for Player 2 to win using Group 00-02.
+        self.assertNotIn(TestTicTacToeHasher.GROUP_00_TO_02, self.hasher.groups_by_squares[1][0][2])
+
+    def test_init_with_indifferent_squares(self):
+        self.env.state = np.array([
+            [
+                [1, 0, 1, ],
+                [1, 0, 0, ],
+                [0, 0, 0, ],
+            ],
+            [
+                [0, 1, 0, ],
+                [0, 1, 0, ],
+                [1, 0, 0, ],
+            ],
+        ])
+        self.hasher = TicTacToeHasher(env=self.env)
+
+        # Validate expected groups at 00. Neither player should be able to win.
+        self.assertFalse(self.hasher.groups_by_squares[0][0][0])
+        self.assertEqual(SquareType.Indifferent, self.hasher.square_types[0][0])
+
+    def test_move(self):
+        # Since no squares have been played, it is possible for Player 2 to win using Group 00-02.
+        self.assertIn(TestTicTacToeHasher.GROUP_00_TO_02, self.hasher.groups_by_squares[1][0][2])
+        self.hasher.move(action=0)
+        # Since Player 1 has played at 00, it is not possible for Player 2 to win using Group 00-02.
+        self.assertNotIn(TestTicTacToeHasher.GROUP_00_TO_02, self.hasher.groups_by_squares[1][0][2])
+        # It should now be Player 2's turn.
+        self.assertEqual(1, self.hasher.player)
 
 
 if __name__ == '__main__':
