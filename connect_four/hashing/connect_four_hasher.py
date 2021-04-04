22311c33a0e2cd13c956d95ec9343cbebba9bbb5
Ryan Pachauri <rpachauri@gmail.com>
2021-04-04 11:31:59 -0700

    Implement ConnectFourHasher.

--- connect_four/hashing/connect_four_hasher.py
+++ connect_four/hashing/connect_four_hasher.py
@@ -1,12 +1,15 @@
+import numpy as np
+
 from connect_four.envs import ConnectFourEnv
-from connect_four.hashing import Hasher
+from connect_four.hashing import Hasher, hasher_hash_utils
 from connect_four.hashing.square_type_manager import SquareTypeManager
 
 
 class ConnectFourHasher(Hasher):
     def __init__(self, env: ConnectFourEnv):
         self.stm = SquareTypeManager(env_variables=env.env_variables, num_to_connect=3)
-        self.lowest_empty_squares = self._find_lowest_empty_squares(state=env.env_variables.state)
+        self.lowest_empty_row_by_col = self._find_lowest_empty_squares(state=env.env_variables.state)
+        self.columns_played_by_move = []
 
     @staticmethod
     def _find_lowest_empty_squares(state):
@@ -30,14 +33,25 @@ class ConnectFourHasher(Hasher):
         Args:
             action (int): a valid action in the current state of Connect-Four.
         """
-        pass
+        # Convert action into (row, col).
+        col = action
+        row = self.lowest_empty_row_by_col[col]
+
+        # Move the SquareTypeManager.
+        self.stm.move(row=row, col=col)
+        # Move the lowest empty row in col up by 1.
+        self.lowest_empty_row_by_col[col] -= 1
+        self.columns_played_by_move.append(col)
 
     def undo_move(self):
         """
         Assumptions:
             1. The current state of Connect-Four is not in the state given upon initialization.
         """
-        pass
+        last_played_column = self.columns_played_by_move.pop()
+        self.lowest_empty_row_by_col[last_played_column] += 1
+
+        self.stm.undo_move()
 
     def hash(self) -> str:
         """
@@ -45,4 +59,13 @@ class ConnectFourHasher(Hasher):
             hash (str): a unique hash of the current state.
                         The encoding is a perfect hash (meaning there will be no collisions).
         """
-        pass
+        transposition_arr = hasher_hash_utils.convert_square_types_to_transposition_arr(
+            square_types=self.stm.square_types,
+        )
+        transposition = hasher_hash_utils.get_transposition(transposition_arr=transposition_arr)
+        flipped = np.fliplr(m=transposition_arr)
+        flipped_transposition = hasher_hash_utils.get_transposition(transposition_arr=flipped)
+        if flipped_transposition < transposition:
+            transposition = flipped_transposition
+
+        return transposition
--- connect_four/hashing/connect_four_hasher_test.py
+++ connect_four/hashing/connect_four_hasher_test.py
@@ -16,7 +16,7 @@ class TestConnectFourHasher(unittest.TestCase):
 
     def test_initialization_initial_state(self):
         want_lowest_empty_squares = [5, 5, 5, 5, 5, 5, 5]
-        self.assertEqual(want_lowest_empty_squares, self.hasher.lowest_empty_squares)
+        self.assertEqual(want_lowest_empty_squares, self.hasher.lowest_empty_row_by_col)
 
     def test_initialization_diagram_6_1(self):
         # This test case is based on Diagram 6.1.
@@ -40,7 +40,62 @@ class TestConnectFourHasher(unittest.TestCase):
         ])
         self.hasher = ConnectFourHasher(env=self.env)
         want_lowest_empty_squares = [5, 5, 4, -1, 4, 5, 5]
-        self.assertEqual(want_lowest_empty_squares, self.hasher.lowest_empty_squares)
+        self.assertEqual(want_lowest_empty_squares, self.hasher.lowest_empty_row_by_col)
+
+    def test_move_undo_move(self):
+        self.hasher.move(action=0)
+        want_columns_played_by_move = [0]
+        self.assertEqual(want_columns_played_by_move, self.hasher.columns_played_by_move)
+
+        self.hasher.undo_move()
+        self.assertFalse(self.hasher.columns_played_by_move)
+
+    def test_hash_flipped_position(self):
+        # This test case is based on Diagram 12.2.
+        self.env.state = np.array([
+            [
+                [0, 0, 0, 0, 0, 0, 0, ],
+                [0, 0, 0, 0, 0, 0, 0, ],
+                [0, 0, 0, 0, 0, 0, 0, ],
+                [0, 0, 0, 0, 0, 0, 0, ],
+                [0, 0, 0, 0, 0, 0, 0, ],
+                [0, 0, 1, 1, 0, 0, 0, ],
+            ],
+            [
+                [0, 0, 0, 0, 0, 0, 0, ],
+                [0, 0, 0, 0, 0, 0, 0, ],
+                [0, 0, 0, 0, 0, 0, 0, ],
+                [0, 0, 0, 0, 0, 0, 0, ],
+                [0, 0, 0, 1, 0, 0, 0, ],
+                [0, 0, 0, 0, 1, 0, 0, ],
+            ],
+        ])
+        self.hasher = ConnectFourHasher(env=self.env)
+        transposition_of_original = self.hasher.hash()
+
+        # Initialize state to a flipped version of the above position.
+        # See Diagram 12.7.
+        self.env.state = np.array([
+            [
+                [0, 0, 0, 0, 0, 0, 0, ],
+                [0, 0, 0, 0, 0, 0, 0, ],
+                [0, 0, 0, 0, 0, 0, 0, ],
+                [0, 0, 0, 0, 0, 0, 0, ],
+                [0, 0, 0, 0, 0, 0, 0, ],
+                [0, 0, 0, 1, 1, 0, 0, ],
+            ],
+            [
+                [0, 0, 0, 0, 0, 0, 0, ],
+                [0, 0, 0, 0, 0, 0, 0, ],
+                [0, 0, 0, 0, 0, 0, 0, ],
+                [0, 0, 0, 0, 0, 0, 0, ],
+                [0, 0, 0, 1, 0, 0, 0, ],
+                [0, 0, 1, 0, 0, 0, 0, ],
+            ],
+        ])
+        self.hasher = ConnectFourHasher(env=self.env)
+        transposition_of_flipped = self.hasher.hash()
+        self.assertEqual(transposition_of_original, transposition_of_flipped)
 
 
 if __name__ == '__main__':
