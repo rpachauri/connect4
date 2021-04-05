3b9cd33336382efcbfd1b83d910fe506e882d522
Ryan Pachauri <rpachauri@gmail.com>
2021-04-04 18:16:03 -0700

    Implement the Victor Evaluator.

---
+++ connect_four/evaluation/victor.py
@@ -0,0 +1,31 @@
+from connect_four.agents.victor.game import Board
+from connect_four.agents.victor.evaluator import evaluator
+from connect_four.envs import ConnectFourEnv
+from connect_four.evaluation import ProofStatus, NodeType
+from connect_four.evaluation.simple_evaluator import SimpleEvaluator
+
+
+class Victor(SimpleEvaluator):
+
+    def __init__(self, model: ConnectFourEnv):
+        super().__init__(model=model)
+
+    def evaluate(self) -> ProofStatus:
+        proof_status = super().evaluate()
+        if proof_status != ProofStatus.Unknown:
+            return proof_status
+
+        board = Board(env_variables=self.model.env_variables)
+        evaluation = evaluator.evaluate(board=board)
+        if evaluation is not None:
+            if self.node_type == NodeType.OR:
+                # If it is White's turn and the evaluation is not None,
+                # then Black has found a way to disprove this node.
+                return ProofStatus.Disproven
+            else:
+                # If it is Black's turn and the evaluation is not None,
+                # then White has found a way to prove this node.
+                return ProofStatus.Proven
+
+        # If the evaluation is None, then this state is unknown.
+        return ProofStatus.Unknown
