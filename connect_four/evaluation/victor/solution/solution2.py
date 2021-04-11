bca033a81a286fa9ff75e98ea5f2e0f07ad0629d
Ryan Pachauri <rpachauri@gmail.com>
2021-04-10 17:51:12 -0700

    Change solution2 module to use the from_* design pattern instead of the initialization pattern. Implement solution2.from_baseinverse().

--- connect_four/evaluation/victor/solution/solution2.py
+++ connect_four/evaluation/victor/solution/solution2.py
@@ -1,30 +1,51 @@
-from connect_four.evaluation.victor.rules import Claimeven, Rule
+from typing import Sequence, Iterable
+
+from connect_four.evaluation.victor.rules import Claimeven, Rule, Baseinverse
+from connect_four.game import Square
 
 
 class Solution:
-    """A Solution is an application of a Rule that refutes at least one group.
+    """A Solution is an application of a Rule.
 
     Two Solutions may or may not work together depending on which squares each
     consists of and which rule they are an application of.
     """
-    def __init__(self, rule_instance: Rule):
+
+    def __init__(self, rule_instance: Rule, squares: Iterable[Square],
+                 claimeven_bottom_squares: Iterable[Square] = None):
         self.rule_instance = rule_instance
-        self.squares = frozenset()
-        self.claimeven_bottom_squares = frozenset()
 
-        if isinstance(self.rule_instance, Claimeven):
-            self._from_claimeven(claimeven=self.rule_instance)
+        self.squares = frozenset(squares)
 
-    def _from_claimeven(self, claimeven: Claimeven):
-        """Initializes Claimeven into a Solution.
+        if claimeven_bottom_squares is None:
+            claimeven_bottom_squares = set()
+        self.claimeven_bottom_squares = frozenset(claimeven_bottom_squares)
 
-        Args:
-            claimeven (Claimeven): a Claimeven.
-
-        Returns:
-            solution (Solution): a Solution.
-        """
-        self.squares = frozenset([claimeven.upper, claimeven.lower])
-        self.claimeven_bottom_squares = frozenset(claimeven.lower)
+    def __eq__(self, other):
+        if isinstance(other, Solution):
+            return (self.rule_instance == other.rule_instance and
+                    self.squares == other.squares and
+                    self.claimeven_bottom_squares == other.claimeven_bottom_squares)
 
 
+def from_claimeven(claimeven: Claimeven) -> Solution:
+    """Converts a Claimeven into a Solution.
+
+    Args:
+        claimeven (Claimeven): a Claimeven.
+
+    Returns:
+        solution (Solution): a Solution.
+    """
+    return Solution(
+        rule_instance=claimeven,
+        squares=[claimeven.upper, claimeven.lower],
+        claimeven_bottom_squares=[claimeven.lower],
+    )
+
+
+def from_baseinverse(baseinverse: Baseinverse) -> Solution:
+    return Solution(
+        rule_instance=baseinverse,
+        squares=baseinverse.squares,
+    )
--- connect_four/evaluation/victor/solution/solution2_test.py
+++ connect_four/evaluation/victor/solution/solution2_test.py
@@ -1,19 +1,31 @@
 import unittest
 
-from connect_four.evaluation.victor.rules import Claimeven
-from connect_four.evaluation.victor.solution.solution2 import Solution
+from connect_four.evaluation.victor.rules import Claimeven, Baseinverse
+from connect_four.evaluation.victor.solution import solution2
 from connect_four.game import Square
 
 
 class TestSolution(unittest.TestCase):
     def test_from_claimeven(self):
         claimeven_2_4 = Claimeven(upper=Square(row=2, col=4), lower=Square(row=3, col=4))
-        solution = Solution(rule_instance=claimeven_2_4)
+        want_solution = solution2.Solution(
+            rule_instance=claimeven_2_4,
+            squares=[claimeven_2_4.upper, claimeven_2_4.lower],
+            claimeven_bottom_squares=[claimeven_2_4.lower],
+        )
+        got_solution = solution2.from_claimeven(claimeven=claimeven_2_4)
+        self.assertEqual(want_solution, got_solution)
 
-        # Validate Solution fields.
-        self.assertEqual(claimeven_2_4, solution.rule_instance)
-        self.assertEqual(frozenset([claimeven_2_4.upper, claimeven_2_4.lower]), solution.squares)
-        self.assertEqual(frozenset(claimeven_2_4.lower), solution.claimeven_bottom_squares)
+    def test_from_baseinverse(self):
+        # The Baseinverse a1-b1 solves a1-d1.
+        baseinverse_a1_b1 = Baseinverse(playable1=Square(row=5, col=0), playable2=Square(row=5, col=1))
+
+        got_solution = solution2.from_baseinverse(baseinverse=baseinverse_a1_b1)
+        want_solution = solution2.Solution(
+            rule_instance=baseinverse_a1_b1,
+            squares=frozenset(baseinverse_a1_b1.squares),
+        )
+        self.assertEqual(want_solution, got_solution)
 
 
 if __name__ == '__main__':
