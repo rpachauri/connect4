3e340ecab89ec47d5f2b413027aed4abe1fb7646
Ryan Pachauri <rpachauri@gmail.com>
2021-04-12 15:00:35 -0700

    Shift the Victor Evaluator to use the GraphManager instead of the evaluator.

--- connect_four/evaluation/victor_evaluator.py
+++ connect_four/evaluation/victor_evaluator.py
@@ -3,6 +3,9 @@ from connect_four.evaluation.victor.evaluator import evaluator
 from connect_four.envs import ConnectFourEnv
 from connect_four.evaluation import ProofStatus, NodeType
 from connect_four.evaluation.simple_evaluator import SimpleEvaluator
+from connect_four.evaluation.victor.graph.graph_manager import GraphManager
+from connect_four.evaluation.victor.solution import VictorSolutionManager
+from connect_four.problem import ConnectFourProblemManager
 
 
 class Victor(SimpleEvaluator):
@@ -15,8 +18,17 @@ class Victor(SimpleEvaluator):
         if proof_status != ProofStatus.Unknown:
             return proof_status
 
-        board = Board(env_variables=self.model.env_variables)
-        evaluation = evaluator.evaluate(board=board)
+        problem_manager = ConnectFourProblemManager(env_variables=self.model.env_variables)
+        solution_manager = VictorSolutionManager(env_variables=self.model.env_variables)
+        graph_manager = GraphManager(
+            player=self.model.env_variables.player_turn,
+            problem_manager=problem_manager,
+            solution_manager=solution_manager,
+        )
+        evaluation = graph_manager.evaluate()
+
+        # board = Board(env_variables=self.model.env_variables)
+        # evaluation = evaluator.evaluate(board=board)
         if evaluation is not None:
             if self.node_type == NodeType.OR:
                 # If it is White's turn and the evaluation is not None,
